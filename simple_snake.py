62323f5379e1df1fb98aa80eba21c9ac6e972faf
NexushasTaken <nexushastaken@gmail.com>
2025-05-10 01:17:50 +0800

    update syntax

--- simple_snake.py
+++ simple_snake.py
@@ -9,53 +9,53 @@ WHITE = (255, 255, 255)
 GREEN = (0, 200, 0)
 RED = (200, 0, 0)
 
-snake = [py.Vector2(100, 100)]
+snake = [py.Vector2(100, 100), py.Vector2(99, 100), py.Vector2(98, 100)]
 direction = py.Vector2(1, 0)  # normalized
 speed = 10
 food = py.Vector2(300, 200)
 
 def draw():
-    screen.fill(WHITE)
-    for block in snake:
-        py.draw.rect(screen, GREEN, (*block, 10, 10))
-    py.draw.rect(screen, RED, (*food, 10, 10))
-    py.display.flip()
+  screen.fill(WHITE)
+  for block in snake:
+    py.draw.rect(screen, GREEN, (*block, 10, 10))
+  py.draw.rect(screen, RED, (*food, 10, 10))
+  py.display.flip()
 
 def random_food():
-    x = random.randrange(0, 600, 10)
-    y = random.randrange(0, 400, 10)
-    return py.Vector2(x, y)
+  x = random.randrange(0, 600, 10)
+  y = random.randrange(0, 400, 10)
+  return py.Vector2(x, y)
 
 running = True
 while running:
-    clock.tick(15)
+  clock.tick(15)
 
-    for event in py.event.get():
-        if event.type == py.QUIT or (event.type == py.KEYDOWN and event.key == py.K_ESCAPE):
-            running = False
-        if event.type == py.KEYDOWN:
-            if event.key == py.K_UP and direction != py.Vector2(0, 1):
-                direction = py.Vector2(0, -1)
-            elif event.key == py.K_DOWN and direction != py.Vector2(0, -1):
-                direction = py.Vector2(0, 1)
-            elif event.key == py.K_LEFT and direction != py.Vector2(1, 0):
-                direction = py.Vector2(-1, 0)
-            elif event.key == py.K_RIGHT and direction != py.Vector2(-1, 0):
-                direction = py.Vector2(1, 0)
+  for event in py.event.get():
+    if event.type == py.QUIT or (event.type == py.KEYDOWN and event.key == py.K_ESCAPE):
+      running = False
+    if event.type == py.KEYDOWN:
+      if event.key == py.K_UP and direction != py.Vector2(0, 1):
+        direction = py.Vector2(0, -1)
+      elif event.key == py.K_DOWN and direction != py.Vector2(0, -1):
+        direction = py.Vector2(0, 1)
+      elif event.key == py.K_LEFT and direction != py.Vector2(1, 0):
+        direction = py.Vector2(-1, 0)
+      elif event.key == py.K_RIGHT and direction != py.Vector2(-1, 0):
+        direction = py.Vector2(1, 0)
 
-    new_head = snake[0] + direction * speed
+  new_head = snake[0] + direction * speed
 
-    if new_head in snake or not (0 <= new_head.x < 600 and 0 <= new_head.y < 400):
-        running = False
-        break
+  if new_head in snake or not (0 <= new_head.x < 600 and 0 <= new_head.y < 400):
+    running = False
+    break
 
-    snake.insert(0, new_head)
+  snake.insert(0, new_head)
 
-    if new_head == food:
-        food = random_food()
-    else:
-        snake.pop()
+  if new_head == food:
+    food = random_food()
+  else:
+    snake.pop()
 
-    draw()
+  draw()
 
 py.quit()
